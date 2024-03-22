5155cdb72fedadfdf2f97138848c171d22481a43
sakshi <sakshi-mishra@csgroupchd.com>
2024-03-22 15:16:33 +0530

    change

--- index.js
+++ index.js
@@ -1,72 +1,60 @@
-// require("dotenv").config();
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const app = express();
-// const cors = require("cors");
-
-// app.use(cors());
-// const port = process.env.PORT || 6867;
-// app.use(express.json());
-// const product_route = require("./routing/routes");
-// const users = require("./schema/register");
-// const { Chats } = require("./schema/Chat");
-
-// app.use("/.netlify/functions-internal/api/products", product_route);
-// const io = require("socket.io")(7654, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-// const storedata = {};
-// io.on("connection", (socket) => {
-//   console.log("connected with user");
-
-//   socket.on("user_connected", async (data) => {
-//     if (data) {
-//       storedata[data.userId] = socket.id;
-//     }
-//   });
-//   socket.on("sendMessage", async (data) => {
-//     const receiverId = storedata[data.receiverId];
-//     const senderId = storedata[data.senderId];
-//     const id1 = data.receiverId;
-//     const userId = data.senderId;
-
-//     const user1 = await users.findById({ _id: id1 });
-//     const user2 = await users.findById({ _id: userId });
-//     const getallmessage = await Chats.aggregate([
-//       { $match: { $or: [{ To: user1.id }, { From: user1.id }] } },
-//       { $match: { $or: [{ To: user2.id }, { From: user2.id }] } },
-//     ]);
-//     io.to(receiverId).emit("receiveMessage", getallmessage);
-//     io.to(senderId).emit("receiveMessage", getallmessage);
-//   });
-
-//   socket.on("disconnect", () => {
-//     const userId = Object.keys(storedata).find(
-//       (key) => storedata[key] === socket.id
-//     );
-//     if (userId) {
-//       delete storedata[userId];
-//       console.log(`User ${userId} disconnected`);
-//     }
-//   });
-// });
-
-// app.listen(port ,async()=>{
-//   await mongoose.connect(process.env.MONGO_URL)
-// })
-
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
+require("dotenv").config();
 const express = require("express");
-const router = express.Router();
-const api = express();
-const serverless = require("serverless-http");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
 
-router.get("/hello", (req, res) => res.send("Hello World!"));
+app.use(cors());
+const port = process.env.PORT || 6867;
+app.use(express.json());
+const product_route = require("./routing/routes");
+const users = require("./schema/register");
+const { Chats } = require("./schema/Chat");
 
-api.use("/api/", router);
+app.use("/.netlify/functions-internal/api/products", product_route);
+const io = require("socket.io")(7654, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+const storedata = {};
+io.on("connection", (socket) => {
+  console.log("connected with user");
 
-serverless(api);
+  socket.on("user_connected", async (data) => {
+    if (data) {
+      storedata[data.userId] = socket.id;
+    }
+  });
+  socket.on("sendMessage", async (data) => {
+    const receiverId = storedata[data.receiverId];
+    const senderId = storedata[data.senderId];
+    const id1 = data.receiverId;
+    const userId = data.senderId;
+
+    const user1 = await users.findById({ _id: id1 });
+    const user2 = await users.findById({ _id: userId });
+    const getallmessage = await Chats.aggregate([
+      { $match: { $or: [{ To: user1.id }, { From: user1.id }] } },
+      { $match: { $or: [{ To: user2.id }, { From: user2.id }] } },
+    ]);
+    io.to(receiverId).emit("receiveMessage", getallmessage);
+    io.to(senderId).emit("receiveMessage", getallmessage);
+  });
+
+  socket.on("disconnect", () => {
+    const userId = Object.keys(storedata).find(
+      (key) => storedata[key] === socket.id
+    );
+    if (userId) {
+      delete storedata[userId];
+      console.log(`User ${userId} disconnected`);
+    }
+  });
+});
+
+app.listen(port ,async()=>{
+  await mongoose.connect(process.env.MONGO_URL)
+})
+
