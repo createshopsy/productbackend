71803bed7e60a85d6677f06d6f708e282bb50ff9
sakshi <sakshi-mishra@csgroupchd.com>
2024-04-04 10:57:31 +0530

    new

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 
-app.use(cors("*"));
+app.use(cors("https://frontend-mu-indol.vercel.app"));
 const port = process.env.PORT || 6867;
 app.use(express.json());
 const product_route = require("./routing/routes");
